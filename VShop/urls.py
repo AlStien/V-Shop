00c848ef33c6d357b615b48af71d61a1a9c81ab2
AlStien <aj601604@gmail.com>
2021-10-28 21:53:14 +0530

    basic django layout

--- VShop/urls.py
+++ VShop/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import base, path, include
+from django.urls import path, include
 
 urlpatterns = [
-    path('', include(base.urls)),
+    path('', include('base.urls')),
     path('admin/', admin.site.urls),
 ]
