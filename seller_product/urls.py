5e2d0fca55bbd22e540dee8e1840a2fd3771705e
Ayush Jain <76158814+AlStien@users.noreply.github.com>
2021-11-14 23:52:32 +0530

    Update urls.py

--- seller_product/urls.py
+++ seller_product/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProductView.as_view()),
+    path('', views.ProductsView.as_view()),
+    path('add-product/', views.ProductView.as_view()),
     path('add-comment/', views.Comment_add_api.as_view()),
     path('add-tag/', views.Tag_add_api.as_view()),
     path('wishlist/', views.WishlistView.as_view()),
