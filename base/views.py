b42e98d3e543c1bcfc485f86ebd767723052f52f
Akshay-Bhatia22 <91560853+Akshay-Bhatia22@users.noreply.github.com>
2021-11-27 17:29:43 +0530

    bug fix revert

--- base/views.py
+++ base/views.py
@@ -224,19 +224,19 @@ class PasswordChangeView(APIView):
             return Response(message, status=status.HTTP_406_NOT_ACCEPTABLE)
 
 
-    # permission_classes = [IsAuthenticated,]
-    # def put(self, request, format=None):
+    permission_classes = [IsAuthenticated,]
+    def put(self, request, format=None):
         
-    #     email = request.user.email
+        email = request.user.email
 
-    #     if OTP.objects.filter(otpEmail__iexact = email).exists():
-    #         if NewUser.objects.filter(email__iexact = email).exists():
-    #             user = NewUser.objects.get(email__iexact = request.user.email)
-    #             user.is_seller = True
-    #             user.save()
-    #             serializer = ProfileSerializer(user, many=False)
-    #             return Response(serializer.data)  
-    #     return Response(message = {'message':'incorrect credentials'}, status=status.HTTP_204_NO_CONTENT)  
+        if OTP.objects.filter(otpEmail__iexact = email).exists():
+            if NewUser.objects.filter(email__iexact = email).exists():
+                user = NewUser.objects.get(email__iexact = request.user.email)
+                user.is_seller = True
+                user.save()
+                serializer = ProfileSerializer(user, many=False)
+                return Response(serializer.data)  
+        return Response(message = {'message':'incorrect credentials'}, status=status.HTTP_204_NO_CONTENT)  
 
 # ------ Prime Membership -------
 class PrimeMember(APIView):
